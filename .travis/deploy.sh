7ce81bf8a3824380938d01c377a1d7c0fca3bac3
Ray <sbmzhcn@gmail.com>
2018-07-26 11:58:16 +0800

    upate travis config

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,12 +3,16 @@ set -ev
 export TZ='Asia/Shanghai'
 
 # 先 clone 再 commit，避免直接 force commit
-git clone -b master git@github.com:sbmzhcn/resume.git
-
+# git clone -b master git@github.com:sbmzhcn/resume.git
 cd resume
+
+# start create pdf
 touch resume.tex
 xelatex resume.tex
 rm -rf *.log *.aux *.out *.fls *.fdb_latexmk *.gz
+
+# start commit
+cd ..
 
 git add .
 git commit -m "Resume updated: `date +"%Y-%m-%d %H:%M:%S"`"

f40f303cce6e3e5f1b1f809edb43145f5fd5bd3b
Ray Chang <love4026@gmail.com>
2019-02-12 22:44:10 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -33,5 +33,5 @@ git commit -m "resume.pdf updated: `date +"%Y-%m-%d %H:%M:%S"` [ci skip]"
 git push --force --quiet "https://yzv:${CO_TOKEN}@git.dev.tencent.com/yzv/yzv.coding.me.git" master:master
 
 # push to ftp
-# ncftpput -R -v -u "$FTP_USER" -p "$FTP_PASS" $FTP_HOST / ../build/resume.pdf
 ping -c 4 $FTP_HOST
+ncftpput -R -v -u "$FTP_USER" -p "$FTP_PASS" $FTP_HOST / ../build/resume.pdf

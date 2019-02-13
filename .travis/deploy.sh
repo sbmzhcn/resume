034bad8bbaa5e007546051e0f601b116c445b71f
Ray <sbmzhcn@gmail.com>
2019-02-13 13:34:42 +0800

    first add front-end resume type

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -18,16 +18,22 @@ cd resume
 touch resume.tex
 xelatex resume.tex -interaction=nonstopmode
 xelatex resume.tex -interaction=nonstopmode
+
+# create front-end pdf
+touch resume-front-end.tex
+xelatex resume-front-end.tex -interaction=nonstopmode
+xelatex resume-front-end.tex -interaction=nonstopmode
+
 rm -rf *.log *.aux *.out *.fls *.fdb_latexmk *.gz
 
 # move to build dir
 mkdir build
-mv resume.pdf build/
+mv resume*.pdf build/
 
 # push to coding
 git clone https://git.dev.tencent.com/yzv/yzv.coding.me.git
 cd yzv.coding.me
-cp ../build/resume.pdf resume/
+cp ../build/*.pdf resume/
 git add .
 git commit -m "resume.pdf updated: `date +"%Y-%m-%d %H:%M:%S"` [ci skip]"
 git push --force --quiet "https://yzv:${CO_TOKEN}@git.dev.tencent.com/yzv/yzv.coding.me.git" master:master

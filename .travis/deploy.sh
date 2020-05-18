ee275aa474e844607476f4005ebef2c4d9491ff1
sbmzhcn <sbmzhcn@gmail.com>
2020-05-18 12:12:24 +0800

    :green_heart: 不再向coding.net推送，因为总是失败

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -31,12 +31,12 @@ mkdir build
 mv resume*.pdf build/
 
 # push to coding
-git clone https://git.dev.tencent.com/yzv/yzv.coding.me.git
-cd yzv.coding.me
-cp ../build/*.pdf resume/
-git add .
-git commit -m "resume.pdf updated: `date +"%Y-%m-%d %H:%M:%S"` [ci skip]"
-git push --force --quiet "https://yzv:${CO_TOKEN}@git.dev.tencent.com/yzv/yzv.coding.me.git" master:master
+# git clone https://git.dev.tencent.com/yzv/yzv.coding.me.git
+# cd yzv.coding.me
+# cp ../build/*.pdf resume/
+# git add .
+# git commit -m "resume.pdf updated: `date +"%Y-%m-%d %H:%M:%S"` [ci skip]"
+# git push --force --quiet "https://yzv:${CO_TOKEN}@git.dev.tencent.com/yzv/yzv.coding.me.git" master:master
 
 # push to ftp
 # ping -c 4 $FTP_HOST
